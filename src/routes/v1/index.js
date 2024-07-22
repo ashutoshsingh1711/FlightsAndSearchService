97232f38adc1b831a9d0f64eb246a7740ce701e0
unknown <ashutoshsingh1711@gmail.com>
2024-07-22 15:43:01 +0530

    Added create flight validation middleware

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { FlightMiddlewares } = require("../../middlewares/index");
 const CityController = require("../../controllers/city-controller");
 const FlightController = require("../../controllers/flight-controller");
 const AirportController = require("../../controllers/airport-controller");
@@ -10,7 +11,11 @@ router.patch("/city/:id", CityController.update);
 router.get("/city/:id", CityController.get);
 router.get("/city", CityController.getAll);
 
-router.post("/flights", FlightController.create);
+router.post(
+  "/flights",
+  FlightMiddlewares.validateCreateFlight,
+  FlightController.create
+);
 router.get("/flights", FlightController.getAll);
 
 router.post("/airports", AirportController.create);
